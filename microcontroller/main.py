abb902205fcb4f3106edc24769290817b2f1fe41
Robert Lewicki <contact@rlewicki.tech>
2024-12-05 07:27:37 +0100

    Display agenda items that fit onto single screen, using small font for dates and big font for item description

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -9,18 +9,38 @@ from color_setup import ssd
 from gui.core.writer import Writer
 from gui.core.nanogui import refresh
 from gui.widgets.label import Label
-import gui.fonts.freesans20 as freesans20
+import gui.fonts.arial10 as arial10
+import gui.fonts.courier20 as courier20
 
 class AgendaEntry:
     def __init__(self) -> None:
-        self.date = {}
-        self.time = {}
-        self.description = {}
+        self.day = -1
+        self.month = -1
+        self.year = -1
+        self.time = None
+        self.description = None
+
+month_names = [
+    "Styczen",
+    "Luty",
+    "Marzec",
+    "Kwiecien",
+    "Maj",
+    "Czerwiec",
+    "Lipiec",
+    "Sierpien",
+    "Wrzesien",
+    "Pazdziernik",
+    "Listopad",
+    "Grudzien"
+]
 
 led_pin = machine.Pin('LED', Pin.OUT)
 caldav_username = ""
 caldav_password = ""
 caldav_uri = ""
+caldav_port = ""
+agenda = []
 
 def connect_to_wifi(ssid, password):
     wlan = network.WLAN(network.STA_IF)
@@ -75,7 +95,10 @@ def get_agenda_data(caldav_url):
     for event in events:
         new_entry = AgendaEntry()
         date = event['start'].split()
-        new_entry.date = date[0]
+        date_components = date[0].split('/')
+        new_entry.day = int(date_components[1])
+        new_entry.month = int(date_components[0])
+        new_entry.year = int(date_components[2])
         new_entry.time = date[1]
         new_entry.description = event['summary']
         agenda.append(new_entry)
@@ -85,6 +108,36 @@ def get_current_date():
     current_time = time.localtime()
     current_time = "{}:{}:{}".format(current_time[3] + 1, current_time[4], current_time[5])
     return current_time;
+
+def update_agenda():
+    # Obviously passing username and password as URL parameters is not safe but this is all supposed to stay within
+    # local network so I don't really care about anyone seeing this.
+    global agenda
+    caldav_request_uri = "http://" + caldav_uri + ":" + caldav_port + "/agenda?username=" + caldav_username + "&password=" + caldav_password
+    print("fetching agenda from calendar...")
+    try:
+        agenda = get_agenda_data(caldav_request_uri)
+        agenda.sort(key=lambda x: (x.year, x.month, x.day, x.time))
+        return True
+    except:
+        print("failed to fetch new agenda")
+        return False
+    
+def display_agenda():
+    print(f"displaying {len(agenda)} agenda items...")
+    refresh(ssd, True)
+    ssd.wait_until_ready()
+    row = 6
+    for entry in agenda:
+        Label(wri_small_font, row, 0, f"{entry.day} {month_names[entry.month - 1]} {entry.year} {entry.time}")
+        row += arial10.height()
+        Label(wri_big_font, row, 0, entry.description)
+        row += courier20.height()
+        if row + arial10.height() > ssd.height:
+            break
+    refresh(ssd)
+    ssd.wait_until_ready()
+    ssd.sleep()
     
 def boot_sequence():
     print("booting up...")
@@ -97,26 +150,19 @@ def boot_sequence():
 
 boot_sequence()
 print("initializing display...")
-wri = Writer(ssd, freesans20, verbose=False)
-wri.set_clip(False, False, False)
+wri_small_font = Writer(ssd, arial10, verbose=False)
+wri_small_font.set_clip(False, False, False)
+
+wri_big_font = Writer(ssd, courier20, verbose=False)
+wri_big_font.set_clip(False, False, False)
+
 refresh(ssd, True)
 ssd.wait_until_ready()
 
-# Obviously passing username and password as URL parameters is not safe but this is all supposed to stay within
-# local network so I don't really care about anyone seeing this.
 caldav_username, caldav_password, caldav_uri, caldav_port = read_caldav_credentials()
-caldav_request_uri = "http://" + caldav_uri + ":" + caldav_port + "/agenda?username=" + caldav_username + "&password=" + caldav_password
-agenda = get_agenda_data(caldav_request_uri)
-
-row = 5
-for entry in agenda:
-    Label(wri, row, 0, f"{entry.date} {entry.time}")
-    row += freesans20.height()
-    Label(wri, row, 0, entry.description)
-    row += freesans20.height()
-refresh(ssd)
-ssd.wait_until_ready()
-ssd.sleep()
+success = update_agenda()
+if success:
+    display_agenda()
 
 def loop():
     current_date = get_current_date()
