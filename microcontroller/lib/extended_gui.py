4495ebfd94db8a7dc499fd2b0d575ea202ebfcca
Robert Lewicki <contact@rlewicki.tech>
2024-12-11 22:39:59 +0100

    Fix case where width could be smaller than text_width for PicoLabel

--- microcontroller/lib/extended_gui.py
+++ microcontroller/lib/extended_gui.py
@@ -5,9 +5,9 @@ from gui.widgets.label import ALIGN_RIGHT
 
 class PicoLabel(Label):
     def __init__(self, writer, text, x, y, width = -1, align = ALIGN_CENTER):
-        if width <= 0:
-            width = writer.stringlen(text)
         text_width = writer.stringlen(text)
+        if width <= 0 or width < text_width:
+            width = text_width
         if align == ALIGN_CENTER:
             x = x + (width - text_width) // 2
         elif align == ALIGN_RIGHT:
