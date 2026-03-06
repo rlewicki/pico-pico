f46ceb2777070a31ab098dcb3871571f8ed7c5bf
rlewicki <contact@rlewicki.tech>
2026-03-07 00:48:21 +0100

    Detect whether an event is a whole day event or not inside the CALDAV flask app

--- caldav-http-server/app/app.py
+++ caldav-http-server/app/app.py
@@ -1,6 +1,8 @@
+from pprint import pprint
+
 from flask import Flask
 from flask import request
-import caldav
+from caldav import get_davclient
 import json
 from datetime import datetime
 from datetime import timedelta
@@ -9,50 +11,50 @@ from unidecode import unidecode
 
 def fill_event(component, calendar) -> dict[str, str]:
     cur = {}
-    cur["calendar"] = unidecode(f"{calendar}")
     cur["summary"] = unidecode(component.get("summary"))
     description = component.get("description")
     if description is not None:
         cur["description"] = unidecode(description)
-    cur["start"] = component.get("dtstart").dt.strftime("%m/%d/%Y %H:%M")
+    start_date = component.get("dtstart")
+    cur["start_date"] = start_date.dt.strftime("%m/%d/%Y %H:%M")
+    cur["whole_day_event"] = False
+    if "value" in start_date.params:
+        cur["whole_day_event"] = True
     endDate = component.get("dtend")
     if endDate and endDate.dt:
         cur["end"] = endDate.dt.strftime("%m/%d/%Y %H:%M")
-    cur["datestamp"] = component.get("dtstamp").dt.strftime("%m/%d/%Y %H:%M")
     return cur
 
-caldav_url = os.environ.get("CALDAV_SERVER_URI")
+caldav_url = os.environ.get("CALDAV_URL")
 
 app = Flask(__name__)
 
 @app.route('/agenda')
 def agenda():
-    caldev_username = request.args.get('username')
-    caldev_password = request.args.get('password')
+    caldav_username = request.args.get('username')
+    caldav_password = request.args.get('password')
     days = int(request.args.get('days'))
-    if caldev_username == None or caldev_password == None:
+    if caldav_username == None or caldav_password == None:
         return "failed to provide username or password", 400
-    caldav_client = caldav.DAVClient(
-        url=caldav_url,
-        username=caldev_username,
-        password=caldev_password)
-    if not caldav_client:
-        return "failed to create caldav client", 400
-    principal = caldav_client.principal()
-    calendars = principal.calendars()
-    events = []
-    for calendar in calendars:
-        events_fetched = calendar.search(
-            start=datetime.now(),
-            end=datetime.now() + timedelta(days=days),
-            event=True,
-        )
-        for event in events_fetched:
-            for component in event.icalendar_instance.walk():
-                if component.name != "VEVENT":
-                    continue
-                events.append(fill_event(component, calendar))
-    return json.dumps(events, indent=2, ensure_ascii=False)
+    
+    with get_davclient(url=caldav_url, username=caldav_username, password=caldav_password) as client:
+        client.ssl_verify_cert = False
+        principal = client.principal()
+        calendars = principal.calendars()
+        events = []
+        for calendar in calendars:
+            events_fetched = calendar.search(
+                start=datetime.now(),
+                end=datetime.now() + timedelta(days=days),
+                event=True,
+            )
+            for event in events_fetched:
+                for component in event.icalendar_instance.walk():
+                    if component.name != "VEVENT":
+                        continue
+                    events.append(fill_event(component, calendar))
+        return json.dumps(events, indent=2, ensure_ascii=False)
+    return "failed to create caldav client", 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
