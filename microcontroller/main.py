05c3c3750cefef186e37f5aa0ff29cc73b61b6bb
rlewicki <contact@rlewicki.tech>
2026-03-18 16:51:36 +0100

    Minor linting tweaks and formatting fixes

--- microcontroller/main.py
+++ microcontroller/main.py
@@ -8,17 +8,14 @@ import urequests
 import json
 import forecast_images
 
-from color_setup import ssd
-from gui.core.writer import Writer
-from gui.core.nanogui import refresh
-from gui.widgets.label import Label
-from gui.widgets.label import ALIGN_CENTER
-from gui.widgets.label import ALIGN_LEFT
-from gui.widgets.label import ALIGN_RIGHT
-import gui.fonts.arial10 as arial10
-import gui.fonts.courier20 as courier20
+from lib.color_setup import ssd
+from lib.gui.core.writer import Writer
+from lib.gui.core.nanogui import refresh
+from lib.gui.widgets.label import Label
+import lib.gui.fonts.arial10 as arial10
+import lib.gui.fonts.courier20 as courier20
 
-from extended_gui import PicoLabel
+from lib.extended_gui import PicoLabel
 
 AGENDA_SCREEN = 2
 AGENDA_FAILED = 3
@@ -28,6 +25,7 @@ WEATHER_UPDATING = 5
 WEATHER_FAILED = 8
 INITIALIZING = 7
 
+
 class WeatherEntry:
     def __init__(self) -> None:
         self.date = -1
@@ -35,6 +33,7 @@ class WeatherEntry:
         self.temp_max = -1
         self.precipitation = -1
         self.weather_code = -1
+
 
 class AgendaEntry:
     def __init__(self) -> None:
@@ -48,10 +47,12 @@ class AgendaEntry:
         self.is_whole_day_event = False
         self.description_lines = []
 
+
 class AgendaPage:
     def __init__(self) -> None:
         self.agendaEntries = []
-        
+
+
 class Globals:
     def __init__(self) -> None:
         self.app_state = INITIALIZING
@@ -81,7 +82,7 @@ class Globals:
         self.wlan = network.WLAN(network.STA_IF)
         self.wifi_ssid = ""
         self.wifi_password = ""
-        
+
 
 month_names = [
     "Styczen",
@@ -132,6 +133,7 @@ button = Pin(3, Pin.IN, Pin.PULL_DOWN)
 white_led = Pin(2, Pin.OUT)
 white_led.value(0)
 
+
 def toggle_led(source):
     if g.white_led_value == 1:
         g.white_led_value = 0
@@ -142,9 +144,9 @@ def toggle_led(source):
 
 def start_led_flashing():
     if g.led_counter == 0:
-        g.led_timer.init(period=500, mode=Timer.PERIODIC, callback = toggle_led)
+        g.led_timer.init(period=500, mode=Timer.PERIODIC, callback=toggle_led)
     g.led_counter += 1
-    
+
 
 def stop_led_flashing():
     g.led_counter -= 1
@@ -164,7 +166,7 @@ def disconnect_from_wifi():
 def connect_to_wifi():
     print("connecting to wifi...")
     g.wlan.active(True)
-    g.wlan.config(pm = 0xa11140)
+    g.wlan.config(pm=0xa11140)
     g.wlan.connect(g.wifi_ssid, g.wifi_password)
     while not g.wlan.isconnected():
         print("waiting for Internet connection...")
@@ -176,11 +178,11 @@ def set_current_time():
     ntptime.settime()
     current_time = time.localtime()
     print("{}:{}:{}, {}/{}/{}".format(
-        current_time[3] + 1, 
-        current_time[4], 
-        current_time[5], 
-        current_time[2], 
-        current_time[1], 
+        current_time[3] + 1,
+        current_time[4],
+        current_time[5],
+        current_time[2],
+        current_time[1],
         current_time[0]))
 
 
@@ -251,11 +253,12 @@ def get_agenda_data(caldav_url) -> list[AgendaEntry]:
         agenda.append(new_entry)
     return agenda
 
-        
+
 def get_current_date():
     current_time = time.localtime()
-    current_time = "{}:{}:{}".format(current_time[3] + 1, current_time[4], current_time[5])
-    return current_time;
+    current_time = "{}:{}:{}".format(
+        current_time[3] + 1, current_time[4], current_time[5])
+    return current_time
 
 
 def get_open_meteo_uri():
@@ -269,11 +272,14 @@ def update_agenda():
     # within a local network I'm not to worried about this
     g.app_state = AGENDA_UPDATING
     g.current_agenda_page = 0
-    caldav_request_uri = "http://" + g.caldav_uri + ":" + g.caldav_port + "/agenda?username=" + g.caldav_username + "&password=" + g.caldav_password + "&days=60"
+    caldav_request_uri = "http://" + g.caldav_uri + ":" + g.caldav_port + \
+        "/agenda?username=" + g.caldav_username + \
+        "&password=" + g.caldav_password + "&days=60"
     print("fetching agenda from calendar...")
     try:
         g.agenda = get_agenda_data(caldav_request_uri)
-        g.agenda.sort(key=lambda x: (x.start_year, x.start_month, x.start_day, x.start_time))
+        g.agenda.sort(key=lambda x: (
+            x.start_year, x.start_month, x.start_day, x.start_time))
         update_agenda_paging()
         display_agenda(g.current_agenda_page)
         g.app_state = AGENDA_SCREEN
@@ -316,7 +322,8 @@ def display_agenda(pageIndex):
     agenda_page = g.agenda_pages[pageIndex]
     print(f"displaying {len(agenda_page.agendaEntries)} agenda items...")
     for entry in agenda_page.agendaEntries:
-        Label(g.wri_small_font, row, 0, f"{entry.start_day} {month_names[entry.start_month - 1]} {entry.start_year} {entry.start_time}")
+        Label(g.wri_small_font, row, 0,
+              f"{entry.start_day} {month_names[entry.start_month - 1]} {entry.start_year} {entry.start_time}")
         row += arial10.height()
         for desc_line in entry.description_lines:
             Label(g.wri_big_font, row, 0, desc_line)
@@ -329,7 +336,8 @@ def display_agenda(pageIndex):
 
 
 def fetch_weather_info(uri) -> list[WeatherEntry]:
-    Label(g.wri_big_font, int(ssd.height / 2 - g.wri_big_font.height / 2), 8, "updating forecast")
+    Label(g.wri_big_font, int(ssd.height / 2 -
+          g.wri_big_font.height / 2), 8, "updating forecast")
     refresh(ssd, True)
     response = http_get_request(uri)
     forecast = json.loads(response)
@@ -339,7 +347,7 @@ def fetch_weather_info(uri) -> list[WeatherEntry]:
     current.temp_max = current.temp_min
     current.precipitation = forecast["current"]["precipitation"]
     current.weather_code = forecast["current"]["weather_code"]
-    
+
     daily_data = forecast["daily"]
     tomorrow = WeatherEntry()
     tomorrow.date = daily_data["time"][1]
@@ -347,14 +355,14 @@ def fetch_weather_info(uri) -> list[WeatherEntry]:
     tomorrow.temp_max = daily_data["temperature_2m_max"][1]
     tomorrow.precipitation = daily_data["precipitation_probability_max"][1]
     tomorrow.weather_code = daily_data["weather_code"][1]
-    
+
     day_after_tomorrow = WeatherEntry()
     day_after_tomorrow.date = daily_data["time"][2]
     day_after_tomorrow.temp_min = daily_data["temperature_2m_min"][2]
     day_after_tomorrow.temp_max = daily_data["temperature_2m_max"][2]
     day_after_tomorrow.precipitation = daily_data["precipitation_probability_max"][2]
     day_after_tomorrow.weather_code = daily_data["weather_code"][2]
-    
+
     weather_info = [
         current,
         tomorrow,
@@ -374,9 +382,9 @@ def display_image(pos_x, pos_y, width, height, img_data):
 def update_weather():
     g.app_state = WEATHER_UPDATING
 
-    # We should not only check if the data is empty, but also the last time we updated.
-    # Otherwise we will only ever update the weather on demand rather than periodically without
-    # any user input.
+    # We should not only check if the data is empty, but also the last time we
+    # updated. Otherwise we will only ever update the weather on demand rather
+    # than periodically without any user input.
     if len(g.weather_info) <= 0:
         try:
             g.weather_info = fetch_weather_info(g.open_meteo_uri)
@@ -384,7 +392,8 @@ def update_weather():
             print("failed to fetch weather update:", e)
             refresh(ssd, True)
             ssd.wait_until_ready()
-            PicoLabel(g.wri_small_font, "Failed to fetch the weather update.", 4, 10)
+            PicoLabel(g.wri_small_font,
+                      "Failed to fetch the weather update.", 4, 10)
             PicoLabel(g.wri_small_font, "Press the button to retry.", 4, 20)
             refresh(ssd)
             ssd.wait_until_ready()
@@ -397,31 +406,45 @@ def update_weather():
     forecast_day_after_tomorrow = g.weather_info[2]
     refresh(ssd, True)
     ssd.wait_until_ready()
-    
+
     label_width = ssd.width // 3
     first_row_height = 8
     second_row_height = 25
     third_row_height = 45
     icons_height = 60
     icons_size = 64
- 
+
     PicoLabel(g.wri_small_font, "Now", 0, first_row_height, label_width)
     # Replace Tomorrow and DaT with actual names of the days
-    PicoLabel(g.wri_small_font, "Tomorrow", label_width, first_row_height, label_width)
-    PicoLabel(g.wri_small_font, "DaT", label_width * 2, first_row_height, label_width)
-    
-    PicoLabel(g.wri_big_font, f"{forecast_now.temp_min}C", 0, second_row_height, label_width)
-    PicoLabel(g.wri_big_font, f"{int(forecast_now.precipitation)}%", 0, third_row_height, label_width)
-    display_image(9, icons_height, icons_size, icons_size, weather_code_to_icon[forecast_now.weather_code])
-    
-    PicoLabel(g.wri_big_font, f"{forecast_tomorrow.temp_min}C", label_width, second_row_height, label_width)
-    PicoLabel(g.wri_big_font, f"{forecast_tomorrow.temp_max}C", label_width, third_row_height, label_width)
-    display_image(93, icons_height, icons_size, icons_size, weather_code_to_icon[forecast_tomorrow.weather_code])
-    
-    PicoLabel(g.wri_big_font, f"{forecast_day_after_tomorrow.temp_min}C", label_width * 2, second_row_height, label_width)
-    PicoLabel(g.wri_big_font, f"{forecast_day_after_tomorrow.temp_max}C", label_width * 2, third_row_height, label_width)
-    display_image(176, icons_height, icons_size, icons_size, weather_code_to_icon[forecast_day_after_tomorrow.weather_code])
-    
+    PicoLabel(g.wri_small_font, "Tomorrow", label_width,
+              first_row_height, label_width)
+    PicoLabel(g.wri_small_font, "DaT", label_width *
+              2, first_row_height, label_width)
+
+    PicoLabel(g.wri_big_font, f"{forecast_now.temp_min}C",
+              0, second_row_height, label_width)
+    PicoLabel(g.wri_big_font,
+              f"{int(forecast_now.precipitation)}%",
+              0,
+              third_row_height,
+              label_width)
+    display_image(9, icons_height, icons_size, icons_size,
+                  weather_code_to_icon[forecast_now.weather_code])
+
+    PicoLabel(g.wri_big_font, f"{forecast_tomorrow.temp_min}C",
+              label_width, second_row_height, label_width)
+    PicoLabel(g.wri_big_font, f"{forecast_tomorrow.temp_max}C",
+              label_width, third_row_height, label_width)
+    display_image(93, icons_height, icons_size, icons_size,
+                  weather_code_to_icon[forecast_tomorrow.weather_code])
+
+    PicoLabel(g.wri_big_font, f"{forecast_day_after_tomorrow.temp_min}C",
+              label_width * 2, second_row_height, label_width)
+    PicoLabel(g.wri_big_font, f"{forecast_day_after_tomorrow.temp_max}C",
+              label_width * 2, third_row_height, label_width)
+    display_image(176, icons_height, icons_size, icons_size,
+                  weather_code_to_icon[forecast_day_after_tomorrow.weather_code])
+
     ssd.vline(label_width, 0, ssd.height, 1)
     ssd.vline(label_width * 2, 0, ssd.height, 1)
 
@@ -447,32 +470,40 @@ def button_state_changed(pin):
     DEBOUNCE_TIME = 125
     current_time = time.ticks_ms()
     if pin.value() == 0:
-        time_elapsed_since_release_ms = time.ticks_diff(current_time, g.last_button_release_time)
+        time_elapsed_since_release_ms = time.ticks_diff(
+            current_time, g.last_button_release_time)
         if time_elapsed_since_release_ms < DEBOUNCE_TIME:
             return
-        time_elapsed_since_last_press_ms = time.ticks_diff(current_time, g.last_button_press_time)
-        print("registering button release. time_elapsed_since_last_press_ms =", time_elapsed_since_last_press_ms)
+        time_elapsed_since_last_press_ms = time.ticks_diff(
+            current_time, g.last_button_press_time)
+        print("registering button release. time_elapsed_since_last_press_ms =",
+              time_elapsed_since_last_press_ms)
         g.button_long_press_timer.deinit()
         g.last_button_release_time = current_time
         if g.was_long_press_triggered or g.was_double_press_triggered:
             g.was_long_press_triggered = False
             g.was_double_press_triggered = False
         else:
-            g.button_single_press_timer.init(period=DOUBLE_PRESS_TIME, mode=Timer.ONE_SHOT, callback = button_single_press_callback)
+            g.button_single_press_timer.init(
+                period=DOUBLE_PRESS_TIME, mode=Timer.ONE_SHOT, callback=button_single_press_callback)
     else:
-        time_elapsed_since_press_ms = time.ticks_diff(current_time, g.last_button_press_time)
+        time_elapsed_since_press_ms = time.ticks_diff(
+            current_time, g.last_button_press_time)
         if time_elapsed_since_press_ms < DEBOUNCE_TIME:
             return
         g.last_button_press_time = current_time
-        time_elapsed_since_release_ms = time.ticks_diff(current_time, g.last_button_release_time)
-        print("registering button press. time_since_last_release_ms = ", time_elapsed_since_release_ms)
+        time_elapsed_since_release_ms = time.ticks_diff(
+            current_time, g.last_button_release_time)
+        print("registering button press. time_since_last_release_ms = ",
+              time_elapsed_since_release_ms)
         if time_elapsed_since_release_ms < DOUBLE_PRESS_TIME:
             print("double press detected")
             g.button_single_press_timer.deinit()
             g.was_double_press_triggered = True
             g.button_double_press = True
         else:
-            g.button_long_press_timer.init(period=LONG_PRESS_TIME, mode=Timer.ONE_SHOT, callback = button_long_press_callback)
+            g.button_long_press_timer.init(
+                period=LONG_PRESS_TIME, mode=Timer.ONE_SHOT, callback=button_long_press_callback)
 
 
 def boot_sequence():
@@ -484,14 +515,15 @@ def boot_sequence():
     connect_to_wifi()
     print("setting current time...")
     set_current_time()
-    
+
     print("initializing button handler...")
-    button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=button_state_changed)
-    
+    button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING,
+               handler=button_state_changed)
+
     print("reading open meteo API uri...")
     g.open_meteo_uri = get_open_meteo_uri()
     print(f"open meteo uri: {g.open_meteo_uri}")
-    
+
     print("reading caldav credentials...")
     g.caldav_username, g.caldav_password, g.caldav_uri, g.caldav_port = read_caldav_credentials()
 
@@ -529,7 +561,7 @@ def loop():
     if not any_button_pressed():
         print("no input registered, skipping update loop...")
         return
-    
+
     start_led_flashing()
     connect_to_wifi()
     ssd.init()
@@ -543,7 +575,8 @@ def loop():
             update_weather()
     elif g.app_state == AGENDA_SCREEN:
         if g.button_single_press:
-            g.current_agenda_page = (g.current_agenda_page + 1) % len(g.agenda_pages)
+            g.current_agenda_page = (
+                g.current_agenda_page + 1) % len(g.agenda_pages)
             display_agenda(g.current_agenda_page)
         elif g.button_double_press:
             update_weather()
